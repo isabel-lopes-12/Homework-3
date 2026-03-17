332d5d79ac2046f7a71546b9a3fef7927bb711e0
Isabel Lopes <Isabelengelienlopes@outlook.com>
2026-03-17 11:51:12 +0100

    Update debugging.js

--- debugging.js
+++ debugging.js
@@ -1,1 +1,30 @@
+let score = 0;
+const moles = document.getElementsByClassName("moles");
+let gameInterval;
 
+function showRandomMole() {
+  for (let i = 0; i < moles.length; i++) {
+    moles[i].style.display = "none";
+  }
+
+  const randomIndex = Math.floor(Math.random() * moles.length);
+  moles[randomIndex].style.display = "block";
+}
+
+function whackMole() {
+  score += 1;
+  document.getElementById("score").innerText = score;
+  this.style.display = "none";
+}
+
+function startGame() {
+  score = 0;
+  document.getElementById("score").innerText = score;
+
+  clearInterval(gameInterval);
+  gameInterval = setInterval(showRandomMole, 1000);
+}
+
+for (let i = 0; i < moles.length; i++) {
+  moles[i].addEventListener("click", whackMole);
+}
